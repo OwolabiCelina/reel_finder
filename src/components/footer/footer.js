7faeb62d2095edd727666d5b283266c30e0258e6
Owolabi Celina <108185066+OwolabiCelina@users.noreply.github.com>
2023-04-17 13:40:04 +0100

    completed footer section

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,18 +1,32 @@
 import "./footer.css";
+import {BiCoffeeTogo} from "react-icons/bi";
+import {BsFillEmojiSmileUpsideDownFill} from "react-icons/bs";
+import { useEffect, useState } from "react";
+import {AiFillHeart} from "react-icons/ai";
 
 const Footer = () => {
 
+    const [date, setYearDate] = useState();
     const yearTime = () => {
-        return new Date().getFullYear();
+        setYearDate( new Date().getFullYear());
     }
+    useEffect(() => {
+        yearTime()
+    })
     return (
-        
-                  <div className="footer-bg">
-                    <p>Wanna buy me a coffee </p>
-                    &copy;
-                </div>
-
-    )
+      <div className="footer-bg">
+        <div>
+          <a href="/contact"> Wanna buy me a coffee</a>
+          <BiCoffeeTogo className="coffee-cup" />
+          <BsFillEmojiSmileUpsideDownFill className="smiley-face" />
+        </div>
+        <p className="copy-year">
+          &copy; {date} Developed and designed with
+          <AiFillHeart className="love" />
+          from Banke Owolabi
+        </p>
+      </div>
+    );
 }
 
 
